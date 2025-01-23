31c45c9b4801180a44b0681f31d9a757210b2f87
Luis Angel <destiny72002@outlook.com>
2025-01-23 00:21:36 -0600

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import FloristPortfolio from './components/FloristPortfolio'
 import WorkGallery from './components/WorkGallery'
 
 function App() {
   return (
-    <main>
-      <Routes>
-        <Route path="/" element={<FloristPortfolio />} />
-        <Route path="/work" element={<WorkGallery />} />
-      </Routes>
-    </main>
+    <HashRouter>
+      <main>
+        <Routes>
+          <Route path="/" element={<FloristPortfolio />} />
+          <Route path="/work" element={<WorkGallery />} />
+        </Routes>
+      </main>
+    </HashRouter>
   )
 }
 
